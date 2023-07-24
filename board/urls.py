137e852bb8349b171befdd5b68af4279c1aba79e
eslionekim <kimdmstj0510@naver.com>
2023-07-24 12:34:18 +0900

    게시물 수정,삭제 기능 만들기

--- board/urls.py
+++ board/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from board.views import boards, index, post
+from board.views import boards, delete, index, post,update
 
 
 urlpatterns=[
     path('',boards, name="boards"),
     path('index/',index),
     path('post/',post,name="post"),
+    path('update/<int:pk>/',update,name='update'),
+    path('delete/<int:pk>/',delete,name='delete'),
 ]
--- board/views.py
+++ board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from board.models import boardList
 
@@ -36,3 +36,25 @@ def post(request):
 		request,
   'board/post.html'
 	)
+  
+def update(request, pk):
+    curPost = get_object_or_404(boardList,pk=pk)
+    if request.method == 'POST':
+      curPost.title = request.POST['title']
+      curPost.subtitle = request.POST['subtitle']
+      curPost.content = request.POST['content']
+      curPost.save()
+      return redirect('boards')
+    return render(
+		request,
+  'board/update.html',
+  {
+    'curPost':curPost,
+  },
+	)
+    
+def delete(request, pk):
+  delPost = get_object_or_404(boardList, pk=pk)
+  delPost.delete()
+  return redirect('boards')
+  
